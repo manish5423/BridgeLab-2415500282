ec05e7440dbc941963f3cc184d989e2dbc5b7747
manish5423 <manishsainigvp12@gmail.com>
2025-11-26 17:25:33 +0530

    Delete student-jdbc directory

--- student-jdbc/src/main/java/com/jdbc/student_jdbc/IConnection.java
+++
@@ -1,8 +0,0 @@
-package com.jdbc.student_jdbc;
-import java.sql.Connection;
-
-public interface IConnection {
-
-	    Connection getConnection();
-	
-}
--- student-jdbc/src/main/java/com/jdbc/student_jdbc/IMenu.java
+++
@@ -1,5 +0,0 @@
-package com.jdbc.student_jdbc;
-
-public interface IMenu {
-    void showMenu();
-}
